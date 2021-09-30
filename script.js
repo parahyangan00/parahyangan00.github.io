974c17e0b63b986b7ed3c1afd70948a81a062583
aripbudiman <muhamadarifbudiman22@gmail.com>
2021-09-30 20:47:11 +0700

    update 20:00

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ const pangatikan = document.getElementById('pangatikan');
 const sukaraja = document.getElementById('sukaraja');
 const sariwangi = document.getElementById('sariwangi');
 const manonjaya = document.getElementById('manonjaya');
+const losAngsuran = document.getElementById('losAngsuran');
 const myAlert = document.querySelector('.my-alert');
 
+// kode rahasia cabang
+const kodePasirwangi = 321;
+const kodeCikajang = 444;
+const kodePangatikan = 789;
+const kodeSukaraja = 928;
+const kodeSariwangi = 859;
+const kodeManonjaya = 776;
 
+// pengkondisian
 pasirwangi.addEventListener('click', function () {
     const p1 = prompt('masukan kode cabang:');
-    const kodepsw = 321;
-    if (p1 == kodepsw) {
+    if (p1 == kodePasirwangi) {
         window.location.href = "cashflow/pasirwangi/index.html";
     } else {
         myAlert.classList.remove('d-none');
@@ -19,8 +27,7 @@ pasirwangi.addEventListener('click', function () {
 
 cikajang.addEventListener('click', function () {
     const p2 = prompt('masukan kode cabang:');
-    const kodeckj = 444;
-    if (p2 == kodeckj) {
+    if (p2 == kodeCikajang) {
         window.location.href = "cashflow/cikajang/index.html";
     } else {
         myAlert.classList.remove('d-none');
@@ -29,8 +36,7 @@ cikajang.addEventListener('click', function () {
 
 pangatikan.addEventListener('click', function () {
     const p3 = prompt('masukan kode cabang:');
-    const kodepgn = 789;
-    if (p3 == kodepgn) {
+    if (p3 == kodePangatikan) {
         window.location.href = "cashflow/pangatikan/index.html";
     } else {
         myAlert.classList.remove('d-none');
@@ -39,9 +45,7 @@ pangatikan.addEventListener('click', function () {
 
 sukaraja.addEventListener('click', function () {
     const p4 = prompt('masukan kode cabang');
-    const kodeskj = 928;
-
-    if (p4 == kodeskj) {
+    if (p4 == kodeSukaraja) {
         window.location.href = "cashflow/sukaraja/index.html";
     } else {
         myAlert.classList.remove('d-none');
@@ -50,8 +54,7 @@ sukaraja.addEventListener('click', function () {
 
 sariwangi.addEventListener('click', function () {
     const p5 = prompt('masukan kode cabang:');
-    const kodesrw = 859;
-    if (p5 == kodesrw) {
+    if (p5 == kodeSariwangi) {
         window.location.href = "cashflow/sariwangi/index.html";
     } else {
         myAlert.classList.remove('d-none');
@@ -60,10 +63,28 @@ sariwangi.addEventListener('click', function () {
 
 manonjaya.addEventListener('click', function () {
     const p6 = prompt('masukan kode cabang:');
-    const kodemnj = 776;
-    if (p6 == kodemnj) {
+    if (p6 == kodeManonjaya) {
         window.location.href = "cashflow/manonjaya/index.html";
     } else {
         myAlert.classList.remove('d-none');
     }
+});
+
+losAngsuran.addEventListener('click',function(){
+const p7 = prompt('masukan koede cabang:');
+if(p7 == kodePasirwangi){
+    window.location.href = "los/index.html";
+}else if(p7 == kodeCikajang){
+    window.location.href="los/index2.html";
+}else if(p7 == kodePangatikan){
+    window.location.href="los/index3.html";
+}else if(p7 == kodeSukaraja){
+    window.location.href="los/index4.html";
+}else if(p7 == kodeSariwangi){
+    window.location.href="los/index5.html";
+}else if(p7 == kodeManonjaya){
+    window.location.href="los/index6.html";
+}else{
+    myAlert.classList.remove('d-none');
+}
 });
